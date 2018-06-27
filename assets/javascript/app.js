666de38dded1706d9dce6e623fadb8ede7f93133
KBPF <anfootball@live.com>
2018-06-27 12:35:11 -0700

    Backup

---
+++ assets/javascript/app.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCI9_WJGgY4wSPtz-QfwjhZT_9rOTWS1xk",
+        authDomain: "kpfbootcamp.firebaseapp.com",
+        databaseURL: "https://kpfbootcamp.firebaseio.com",
+        projectId: "kpfbootcamp",
+        storageBucket: "",
+        messagingSenderId: "183430613480"
+    };
+    firebase.initializeApp(config);
+
+    // Variables
+    const database = firebase.database();
+
+    // Button for adding trains
+    $("#add-train-btn").on("click", function (event) {
+        event.preventDefault();
+
+        // Grab user input
+        let trainName = $("#train-name-input").val().trim();
+        let destInput = $("#dest-input").val().trim();
+        let timeInput = $("#time-input").val().trim();
+        let freeInput = $("#free-input").val().trim();
+
+        // Local "filler" object for train data
+        let newTrain = {
+            name: trainName,
+            destination: destInput,
+            time: timeInput,
+            frequency: freeInput
+        };
+
+        // Upload employee data to firebase
+        database.ref().push(newTrain);
+
+        // Console log for all data
+        console.log(newTrain.name);
+        console.log(newTrain.destination);
+        console.log(newTrain.time);
+        console.log(newTrain.frequency);
+
+        // Alert
+        alert("Train successfully added");
+
+        // Clear text-boxes
+        $("train-name-input").val("");
+        $("#dest-input").val("");
+        $("time-input").val("");
+        $("free-input").val("");
+    });
+
+    // Firebase event to add train to table from form
+    database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+        console.log(childSnapshot.val());
+
+        // Store everything 
+        let trainName = childSnapshot.val().name;
+        let destInput = childSnapshot.val().destination;
+        let timeInput = childSnapshot.val().time;
+        let freeInput = childSnapshot.val().frequency;
+
+        // Train
+        console.log(trainName);
+        console.log(destInput);
+        console.log(timeInput);
+        console.log(freeInput);
+
+        // Train Time
+        let trainTimePretty = moment.unix(timeInput).format("HH:mm");
+
+        // Calculate the total time for the train
+        let trainHours = moment().diff(moment(timeInput, "X"), "hours");
+        console.log(trainHours);
+
+        // Calculate the frequency
+        let trainFreq = trainHours * freeInput;
+        console.log(trainFreq);
+
+        // Add each train's data into the table
+        $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destInput + "</td><td>" + trainTimePretty + "</td><td>" + trainHours + "</td><td>" + freeInput + "</td><td>" + trainFreq + "</td><tr>");
+    })
+})
